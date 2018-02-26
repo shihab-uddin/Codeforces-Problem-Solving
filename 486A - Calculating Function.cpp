cf99a7b0f59b5c8c9fbcf91d141d09f6c87b09fb
shihab-uddin <tomdshihab@gmail.com>
2018-02-27 00:06:38 +0600

    Add files via upload

---
+++ 486A - Calculating Function.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n,result;
+    cin>>n;
+    if(n%2==0){
+        result=n/2;
+        cout<<result<<endl;
+    }else{
+        result= -(n+1)/2;
+        cout<<result<<endl;
+    }
+    return 0;
+}
