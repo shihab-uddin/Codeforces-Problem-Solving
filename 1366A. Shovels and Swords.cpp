f1c5d3f5d5b6dbefe658abe24e76ec3c7fc8c0de
Shihab <tomdshihab@gmail.com>
2020-06-13 11:54:50 +0600

    Codeforces

---
+++ 1366A. Shovels and Swords.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int a, b, ans = 0;
+        cin>>a>>b;
+        ans = min(min(a,b), (a+b)/3);
+        cout<<ans<<endl;
+    }
+
+	return 0;
+}
+
